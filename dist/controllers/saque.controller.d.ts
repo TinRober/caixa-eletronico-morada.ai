f48d8f793fc42898af67a17be1e200801c7f9613
Roberto Galarani <galarani.dev@gmail.com>
2025-12-07 12:26:13 -0300

    esqueci de remover o dist tbm

--- dist/controllers/saque.controller.d.ts
+++
@@ -1,5 +0,0 @@
-import { Request, Response } from "express";
-export declare class SaqueController {
-    static realizarSaque(req: Request, res: Response): Response;
-}
-//# sourceMappingURL=saque.controller.d.ts.map
--- dist/utils/httpError.d.ts
+++
@@ -1,5 +0,0 @@
-export declare class HttpError extends Error {
-    readonly statusCode: number;
-    constructor(statusCode: number, message: string);
-}
-//# sourceMappingURL=httpError.d.ts.map
